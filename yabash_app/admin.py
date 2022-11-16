57c63f65d73f57b72549f221537ed802cd19724d
Folarin Ogungbemi <Folarine1@gmail.com>
2022-11-16 20:09:15 +0000

    Setup admin, add Testimonial modal, and install summernote

--- yabash_app/admin.py
+++ yabash_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Testimonial
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Testimonial)
+class TestimonialAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content')
--- yabash_app/models.py
+++ yabash_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Testimonial(models.Model):
+    summary = models.CharField(max_length=100, unique=True)
+    content = models.TextField()
+    name = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="post_testimony")
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"{self.summary} Testimony {self.content} by {self.name}"
