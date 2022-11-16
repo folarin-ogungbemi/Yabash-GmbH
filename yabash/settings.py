a7601a8a7b7757428b806ed13a50ba6ad9e3d098
Folarin Ogungbemi <Folarine1@gmail.com>
2022-11-16 16:08:01 +0000

    Add templates initial

--- yabash/settings.py
+++ yabash/settings.py
@@ -89,7 +89,7 @@ WSGI_APPLICATION = 'yabash.wsgi.application'
 # }
 
 DATABASES = {
-    'default':dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
