5b0e2f6b18178aa09bcebdbe52947435cc6ee026
Folarin Ogungbemi <Folarine1@gmail.com>
2022-11-17 01:13:50 +0000

    Add new line to document

--- yabash_app/views.py
+++ yabash_app/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Headline
 
+
 class Hero(generic.ListView):
     model = Headline
     queryset = Headline.objects.filter(status=1)
     template_name = 'index.html'
-
