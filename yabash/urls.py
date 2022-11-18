35fd4f1b33cf8b7a98dd6042b98f0d70723c9ccb
Folarin Ogungbemi <Folarine1@gmail.com>
2022-11-18 23:24:57 +0000

    install django allauth account verification app

--- yabash/urls.py
+++ yabash/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
     path("", include("yabash_app.urls"), name="homePage"),
     path('summernote/', include('django_summernote.urls')),
 ]
