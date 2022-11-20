0558bde1cab522887fc1c4872b796a10b609d6d4
Folarin Ogungbemi <Folarine1@gmail.com>
2022-11-20 15:42:33 +0000

    format admin

--- yabash_app/admin.py
+++ yabash_app/admin.py
@@ -14,13 +14,29 @@ admin.site.register(Author)
 
 @admin.register(Booking)
 class BookingAdmin(SummernoteModelAdmin):
-    list_display = ('client', 'no_of_guest', 'event_type', 'event_date', 'event_time')
+    list_display = (
+        'client',
+        '  no_of_guest',
+        'event_type',
+        'event_date',
+        'event_time')
     summernote_fields = ('event_info')
-    search_fields = ['client', 'event_type']
-    list_filter = ('event_type', 'event_date', 'no_of_guest')
+    search_fields = [
+        'client',
+        'event_type']
+    list_filter = (
+        'event_type',
+        'event_date',
+        'no_of_guest')
 
 
 @admin.register(Table)
 class TableAdmin(SummernoteModelAdmin):
-    list_display = ('capacity', 'available', 'created_on', 'updated_on')
-    list_filter = ('capacity', 'available',)
+    list_display = (
+        'capacity',
+        'available',
+        'created_on',
+        'updated_on')
+    list_filter = (
+        'capacity',
+        'available',)
