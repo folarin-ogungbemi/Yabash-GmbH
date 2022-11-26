651baf365510cb82e48ca9cc9b34d540d0200442
Folarin Ogungbemi <Folarine1@gmail.com>
2022-11-26 17:11:27 +0000

    Create Readability of Data in BookingRecords and records template

--- yabash_app/urls.py
+++ yabash_app/urls.py
@@ -1,8 +1,9 @@
 from yabash_app import views
 from django.urls import path
-from yabash_app.views import Bookings
+from yabash_app.views import Bookings, BookingRecords
 
 urlpatterns = [
     path("", views.Testimonials.as_view(), name="homePage"),
     path('booking/', views.Bookings.as_view(), name="bookingPage"),
+    path('records/', views.BookingRecords.as_view(), name="bookingRecord"),
 ]
--- yabash_app/views.py
+++ yabash_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
-from .models import Testimonial
+from .models import Testimonial, Booking
 from django.views.generic.edit import FormView
 from .forms import BookingForm
 
@@ -10,6 +10,17 @@ class Testimonials(ListView):
     queryset = Testimonial.objects.filter(status=1)
     template_name = 'index.html'
     context_object_name = 'testimonials'
+
+
+class BookingRecords(ListView):
+    model = Booking
+    form_class = BookingForm
+    template_name = 'records.html'
+    context_object_name = 'records'
+    paginate_by = 3
+
+    def get_queryset(self):
+        return Booking.objects.filter(client=self.request.user.id)
 
 
 class Bookings(FormView):
